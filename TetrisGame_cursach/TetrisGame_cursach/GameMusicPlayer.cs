ea1a8f2ee8aff413f20f02a9702ccf46c71b1d98
K1tsune <60034144+Kitsune424@users.noreply.github.com>
2024-05-15 17:28:27 +0300

    Add Music v3

--- TetrisGame_cursach/TetrisGame_cursach/GameMusicPlayer.cs
+++ TetrisGame_cursach/TetrisGame_cursach/GameMusicPlayer.cs
@@ -1,57 +1,120 @@
 ﻿using System;
 using System.IO;
 using System.Media;
+using System.Windows.Data;
+using System.Windows.Media;
 
 namespace TetrisGame_cursach
 {
+    public class MusicList
+    {
+        private List<string> musicFiles;    // cборник музыки
+        private int ID;                     // Идентификатор трека
+
+        public MusicList()
+        {
+            ID = 0;
+            musicFiles = new List<string>
+            {
+                "Music/Tetris-ElectroSwing.wav",
+                "Music/Tetris-metal.wav",
+                "Music/Tetris-miatriss_remix_.wav",
+                "Music/Tetris-original.wav",
+                "Music/Tetris-phonk.wav"
+            };
+        }
+
+        /// <summary>
+        /// Текущий трек
+        /// </summary>
+        public string GetCurrentFile()
+        {
+            return musicFiles[ID];
+        }
+
+        /// <summary>
+        /// Следующий трек
+        /// </summary>
+        public string GetNextFile()
+        {
+            ID = (ID + 1) % musicFiles.Count;
+            return musicFiles[ID];
+        }
+
+        /// <summary>
+        /// Предыдущий трек
+        /// </summary>
+        public string GetPreviousFile()
+        {
+            ID = (ID - 1 + musicFiles.Count) % musicFiles.Count;
+            return musicFiles[ID];
+        }
+
+        /// <summary>
+        /// Получить случайный трек из очереди
+        /// </summary>
+        public string GetRandomFile()
+        {
+            Random random = new Random();
+            ID = random.Next(0, musicFiles.Count);
+            return musicFiles[ID];
+        }
+
+        public int Count()
+        {
+            return musicFiles.Count;
+        }
+    }
+
     public class GameMusicPlayer
     {
-        private string[] audioFiles;
-        private int ID;
-        private SoundPlayer player;
+        private MediaPlayer player;         // проигрыватель
+        private MusicList musicList;        // cборник музыки
+        private MediaTimeline musicTimeline;
 
-        public GameMusicPlayer(string[] files)
+        public GameMusicPlayer()
         {
-            audioFiles = files;
-            ID = 0;
-            player = new SoundPlayer();
-            player.Stream = new MemoryStream(File.ReadAllBytes(audioFiles[ID]));
-        }
+            player = new MediaPlayer();
+            musicList = new MusicList();
+            player.Volume = 0.2f;
+            player.MediaEnded += (sender, e) =>
+            {
+                PlayTrak(musicList.GetNextFile());
+            };
 
-        public void PlayRandom()
-        {
-            Random rnd = new Random();
-            ID = rnd.Next(0, audioFiles.Length);
-            PlayCurrentFile();
-        }
-
-        public void PlayNext()
-        {
-            ID = (ID + 1) % audioFiles.Length;
-            PlayCurrentFile();
-        }
-
-        public void PlayPrevious()
-        {
-            ID = (ID - 1 + audioFiles.Length) % audioFiles.Length;
-            PlayCurrentFile();
-        }
-
-        public void Stop()
-        {
-            player.Stop();
         }
         
-        public string MusicName()
+        /// <summary>
+        /// Воспроизведение аудио файла
+        /// </summary>
+        /// <param name="filePath"></param>
+        private void PlayTrak(string filePath) 
         {
-            string name = audioFiles[ID];
-            return name;
+            player.Open(new Uri(filePath, UriKind.Relative));
+            player.Play();
         }
 
-        private void PlayCurrentFile()
+        /// <summary>
+        /// Случайное воспроизведение аудиофайла
+        /// </summary>
+        public void GmRandomPlay()
         {
-            player.Stop();
-            player.SoundLocation = audioFiles[ID];
+            PlayTrak(musicList.GetRandomFile());
+        }
+
+        /// <summary>
+        /// Поставить воспроизведение на паузу
+        /// </summary>
+        public void GmPause()
+        {
+            player.Pause();
+        }
+
+        /// <summary>
+        /// Продолжить воспроизведение
+        /// </summary>
+        public void GmContinue(string filePath)
+        {
             player.Play();
         }
     }
